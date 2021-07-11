63d194944649120605f59fcf9535e5e3ea43e9c3
Syz03 <aakibdiscoo@gmail.com>
2021-07-11 20:25:43 +0545

    added the backgroung color for img plus modularized code

--- index.js
+++ index.js
@@ -2,3 +2,8 @@ function myFunction() {
   var popup = document.getElementById("myPopup");
   popup.classList.toggle("show");
 }
+
+function myFunction2(){
+  var backColor = document.getElementById("icon-share");
+  backColor.classList.toggle("color");
+}
